2ce7a24ab39880d799d84a9248fbea437243338f
unknown <cortegamdx@gmail.com>
2024-04-01 22:59:13 -0300

    first commit

---
+++ src/main/java/com/br/algaworks/algafoodapi/AlgafoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.br.algaworks.algafoodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgafoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgafoodApiApplication.class, args);
+	}
+
+}
